49221547e9114af43d5a06977ccc8604d9b3adfd
OmGandhi091611 <omgandhi1611@gmail.com>
2023-04-28 13:57:07 +0530

    minor changes

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { GoogleAuthProvider } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   username : string = '';
-  constructor(private fireauth: AngularFireAuth, private router: Router) { }
+  constructor(private fireauth: AngularFireAuth, private router: Router, private firestore : AngularFirestore) { }
   login(email : string, password : string) {
     this.fireauth.signInWithEmailAndPassword(email , password).then( res => {
       this.router.navigate(['organisation']);
@@ -16,9 +17,12 @@ export class AuthService {
         this.router.navigate(['']);
     })
   }
+
   // register method
   register(email : string, password : string, displayName : string) {
     this.fireauth.createUserWithEmailAndPassword(email , password).then( async res => {
+      const uid = res.user?.uid;
+      const userRef = this.firestore.collection('users').doc(uid);
       await this.updateProfile(displayName);
       this.router.navigate(['organisation']);
     }, err => {
@@ -64,10 +68,12 @@ export class AuthService {
         }).then(() =>{
           this.router.navigate(['./login/username']);
         });
+        console.log(user);
         return;
       } else {
         localStorage.setItem('token', JSON.stringify(uid));
         this.router.navigate(['organisation']);
+        console.log(user);
       }
     });
   };
